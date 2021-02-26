68422ca8cac3fc5b0d685ef45b2a3e906cf63902
rishavpramanik <78920693+rishavpramanik@users.noreply.github.com>
2021-02-27 00:42:22 +0530

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+a=int(input(""))
+if a%2!=0:
+    print("Weird")
+else :
+    if a>1 and a<6:
+        print("Not Weird")
+    if a>5 and a<21:
+        print("Weird")
+    if a>20 :
+        print("Not Weird")
