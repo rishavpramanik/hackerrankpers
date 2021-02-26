72093e60b6bb2be6ea01ee58dd3ee91bb7338731
rishavpramanik <78920693+rishavpramanik@users.noreply.github.com>
2021-02-27 00:27:05 +0530

    Create leftrotation.py

---
+++ leftrotation.py
@@ -0,0 +1,24 @@
+def rotateLeft(d, arr):
+    for i in range(d):
+        a=arr[0]
+        arr.remove(a)
+        arr.append(a)
+    return arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
